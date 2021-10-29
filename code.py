c493b9b44300f518393d4f030e5d541b8a9884db
himanshu singh <rahulsuryavanshi444@gmail.com>
2021-10-30 02:08:30 +0530

    Update code.py

--- code.py
+++ code.py
@@ -6,7 +6,7 @@ Created on Fri Jan 17 19:59:26 2020
 @author: himanshu
 """
 
-# Artificial Neural Network
+
 
 
 
@@ -38,7 +38,7 @@ sc = StandardScaler()
 x_train = sc.fit_transform(x_train)
 x_test = sc.transform(x_test)
 
-# Part 2 - Now let's make the ANN!
+# - Now let's make the ANN!
 
 # Importing the Keras libraries and packages
 import keras
@@ -49,23 +49,23 @@ from keras.layers import Dense
 classifier = Sequential()
 
 # Adding the input layer and the first hidden layer
-classifier.add(Dense(units = 84, kernel_initializer = 'uniform', activation = 'relu', input_dim = 166)) #units perimetr is 84 because we take average of input and output nodes
+classifier.add(Dense(units = 84, kernel_initializer = 'uniform', activation = 'relu', input_dim = 166)) 
 
 # Adding the second hidden layer
-classifier.add(Dense(units = 84, kernel_initializer = 'uniform', activation = 'relu'))#we have used rectifier function in hidden layer
+classifier.add(Dense(units = 84, kernel_initializer = 'uniform', activation = 'relu'))
 
 # Adding the output layer
-classifier.add(Dense(units = 1, kernel_initializer = 'uniform', activation = 'sigmoid')) #we have use sigmoid function in output layer because our problem is on the basis of classifiaction
+classifier.add(Dense(units = 1, kernel_initializer = 'uniform', activation = 'sigmoid'))
 
 # Compiling the ANN
 classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
-                                      #if outcome is not binary then we will use 'categorical_crossentropy loss functiom'
+                                      
 
 # Fitting the ANN to the Training set
 
 history = classifier.fit(x_train, y_train, batch_size = 20, epochs = 80, validation_data=(x_test,y_test))
 
-# Part 3 - Making predictions and evaluating the model
+#  Making predictions and evaluating the model
 
 y_pred = classifier.predict(x_test)
 y_pred = (y_pred>0.5)# more than 50% means prediction is correct
